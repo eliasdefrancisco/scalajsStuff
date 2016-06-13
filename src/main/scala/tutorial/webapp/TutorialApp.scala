7a4fc6ac185841acb03c52fd1a54e0e465194bd4
elias <eliasdefrancisco@gmail.com>
2016-06-13 19:45:45 +0200

    Playing with scalajs tutorial. Until tests point

---
+++ src/main/scala/tutorial/webapp/TutorialApp.scala
@@ -0,0 +1,46 @@
+package tutorial.webapp
+
+/**
+ * Created by elias on 6/13/16.
+ */
+
+import scala.scalajs.js.JSApp
+//import org.scalajs.dom
+//import dom.document
+//import scala.scalajs.js.annotation.JSExport
+import org.scalajs.jquery.jQuery
+
+object TutorialApp extends JSApp {
+
+//  def main(): Unit = {
+//    println("Hello Kitty!")
+//  }
+
+  def main(): Unit = {
+//    appendPar(document.body, "Mr.Scalajs is comming!!")
+//    jQuery("body").append("<p>Mr.Scalajs is comming!!</p>")
+    jQuery(setupUI _)
+  }
+
+//  def appendPar(targetNode: dom.Node, text: String): Unit = {
+//    val parNode = document.createElement("p")
+//    val textNode = document.createTextNode(text)
+//    parNode.appendChild(textNode)
+//    targetNode.appendChild(parNode)
+//  }
+
+//  @JSExport
+  def addClickedMessage(): Unit = {
+//    appendPar(document.body, "You clicked the button, didn't you?")
+    jQuery("body").append("<p>You clicked the button, didn't you?")
+  }
+
+  def setupUI(): Unit = {
+    jQuery("#click-me-button").click(addClickedMessage _)
+    jQuery("body").append("<p>Mr.Scalajs is comming!</p>")
+  }
+
+
+
+}
+
